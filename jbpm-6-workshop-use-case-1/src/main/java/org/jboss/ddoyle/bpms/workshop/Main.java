f7fa385217271b5967003f2149201ff0f7c15c24
Duncan Doyle <Duncan.Doyle@gmail.com>
2016-01-05 18:11:35 +0100

    Added Dockerfile to create a basic BPMSuite 6.2.0 docker images based on the OpenShift EAP 6.4 image. Improved first lab. Added a lot of documentation.

--- jbpm-6-workshop-use-case-1/src/main/java/org/jboss/ddoyle/bpms/workshop/Main.java
+++ jbpm-6-workshop-use-case-1/src/main/java/org/jboss/ddoyle/bpms/workshop/Main.java
@@ -3,11 +3,11 @@ package org.jboss.ddoyle.bpms.workshop;
 import org.drools.core.ClockType;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
-import org.kie.api.runtime.Environment;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.KieSessionConfiguration;
 import org.kie.api.runtime.conf.ClockTypeOption;
+import org.kie.internal.persistence.jpa.JPAKnowledgeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,19 +27,40 @@ public class Main {
 		//Get KieBase.
 		KieBase kieBase = kieContainer.getKieBase();
 		
+		
+		
+		/* *****************************************************************************************************
+		 * 
+		 * In a jBPM application that requires statefulness of long running processes, i.e. processes that contain
+		 * wait-states and need to be stored in datastores, we would need to spin-up a KieSession from the 
+		 * JPAKnowledgeService. A KieSession created via this service would be an implementation of a 
+		 * CommandBasedStatefulKnowledgeSession with a JPA CommandService (SingleSessionCommandService) implementation.
+		 * The SingleSessionCommandService in its turn uses the TransactionInterceptor (which is also a CommandService implementation) 
+		 * to wrap command execution in a (JTA) transaction.
+		 * 
+		 * Also, to create a session using the JPAKnowledgeService, we need to provide a KieBase, a KieSessionConfiguration (e.g. ClockType)
+		 * and an Environment (e.g. TransactionManager, EntityManagerFactory).
+		 * 
+		 * In this example, we will simply use a KieSession without persistence, but we've provided some sample code that
+		 * shows how one would create a KieSession backed by database persistence.
+		 * 
+		 * Please note that in the BPMSuite platform, creating these sessions is done for you behind the scenes.
+		 * This type of code is only needed when one wants to run a process with jBPM embedded in the application (and even in that
+		 * case it's probably easier to utilize the jBPM EJB and/or CDI services. See for example: https://github.com/mswiderski/jbpm-examples/tree/master/jbpm6/jbpm-sample-cdi-services).
+		 * 
+		 *******************************************************************************************************/
 		//Create KieSessionConfiguration
 		//KieSessionConfiguration contains configuration options like ClockType, BeliefSystem, WorkItemHandlers, etc. 
-		KieSessionConfiguration configuration = kieServices.newKieSessionConfiguration();
-		configuration.setOption(ClockTypeOption.get(ClockType.PSEUDO_CLOCK.getId()));
+		//KieSessionConfiguration configuration = kieServices.newKieSessionConfiguration();
+		//configuration.setOption(ClockTypeOption.get(ClockType.PSEUDO_CLOCK.getId()));
 		
 		//Create environment.
 		//Environment contains configuration options like EntityManagerFactory, TransactionManager, etc.
-		Environment environment = kieServices.newEnvironment();
+		//Environment environment = kieServices.newEnvironment();
 		//environment.set(EnvironmentName.ENTITY_MANAGER_FACTORY, myEmf);
 		//environment.set(EnvironmentName.TRANSACTION_MANAGER, myTm);
 		
-		//Retrieve a new CommandBasedStatefulKnowledgeSession with a JPA CommandService (SingleSessionCommandService) implementation.
-		//The SingleSessionCommandService in its turn uses the TransactionInterceptor (which is also a CommandService implementation) to wrap command execution in a transaction.
+		//Create KieSession
 		//KieSession kieSession = JPAKnowledgeService.newStatefulKnowledgeSession(kieBase, configuration, environment);
 		
 		//As we are running this on JavaSE, we will not use any DB or transactions. So this is just a process that runs in memory.
